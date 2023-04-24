ba242aa2e977fc5e651cd589eb8d2828a70d823a
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2023-04-24 02:33:06 -0600

    Pause menu mock UI

--- Assets/Scripts/UI/Menus/AudioMenuUI.cs
+++ Assets/Scripts/UI/Menus/AudioMenuUI.cs
@@ -1,38 +1,23 @@
-using RPG.Control;
 using RPG.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace RPG.UI.Menus
 {
-    public class SettingsMenuUI : MonoBehaviour
+    public class AudioMenuUI : MonoBehaviour
     {
         [SerializeField] SettingsMenu settings;
         [SerializeField] Slider masterVolumeSlider;
         [SerializeField] Slider musicVolumeSlider;
         [SerializeField] Slider sfxVolumeSlider;
 
-        PlayerController playerController;
-
-        void Awake()
-        {
-            playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
-        }
-
-        void OnEnable()
+        void Start()
         {
             SetupSliders();
 
             masterVolumeSlider.onValueChanged.AddListener(settings.SetMasterVolume);
             musicVolumeSlider.onValueChanged.AddListener(settings.SetMusicVolume);
             sfxVolumeSlider.onValueChanged.AddListener(settings.SetSFXVolume);
-        }
-
-        void OnDisable()
-        {
-            masterVolumeSlider.onValueChanged.RemoveListener(settings.SetMasterVolume);
-            musicVolumeSlider.onValueChanged.RemoveListener(settings.SetMusicVolume);
-            sfxVolumeSlider.onValueChanged.RemoveListener(settings.SetSFXVolume);
         }
 
         void SetupSliders()
